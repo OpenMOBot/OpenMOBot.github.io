5e2ff3dfbff7da1cb129d7ffdd0f3d98fa45fd61
majvazov <majvazov@users.noreply.github.com>
2024-10-21 23:31:01 +0000

    deploy: e6ec10db29cb1acbd94fb8230ed94a494d3b7bf9

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-253PT2DX.mjs";import"./chunk-6F2JFXXC.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-NYF2W2WJ.mjs";import"./chunk-KQTOT7AK.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
