f57a4496cb63f527e98f69b018e40edc94706110
majvazov <martin.ayvazov@gmail.com>
2024-10-27 01:42:53 +0300

    Bunch of stupid changes that are fixing the responsiveness

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,20 @@ import { ReviewComponent } from './review/review.component';
 import { StepComponent } from './step/step.component';
 import { BuildPageComponent } from './build-page/build-page.component';
 import { ProgramPageComponent } from './program-page/program-page.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StepComponent, ReviewComponent, BuildPageComponent, ProgramPageComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    StepComponent,
+    ReviewComponent,
+    BuildPageComponent,
+    ProgramPageComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'] // Changed styleUrl to styleUrls (correct spelling)
 })
 export class AppComponent {}
