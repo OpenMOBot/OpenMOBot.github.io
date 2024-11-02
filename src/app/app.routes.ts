ed30856dae07685612e18a1a6e2d676ab389e05a
Azize Arnaud <azize_01@abv.bg>
2024-11-03 00:46:15 +0200

    add about us page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
-import { ReviewComponent } from './review/review.component';
 import { BuildPageComponent } from './build-page/build-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ProgramPageComponent } from './program-page/program-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 export const routes: Routes = [
-  { path: 'review', component: ReviewComponent },
   { path: 'build', component: BuildPageComponent },
   { path: 'program', component: ProgramPageComponent },
+  { path: 'about', component: AboutPageComponent },
   { path: '', component: HomePageComponent },
   { path: '**', redirectTo: '' }  // Redirect invalid paths
 ];
